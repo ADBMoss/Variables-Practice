3312378380dba35582d2bd3f5897019fb1387aad
DarriusB <darrius.bradford@gmail.com>
2022-04-21 12:30:48 -0700

    Practice assigning variables

---
+++ Variables/Variables Practice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myName = "Darrius Bradford";
+            int age = 32;
+            char middleInitial = 'M';
+            bool Ismale = true;
+            decimal Pi = 3.14m;
+            double whyMe = 1.1;
+            float meMe = 2.8f;
+            sbyte small = 11;
+
+            Console.WriteLine($"My name is {myName} and I am {age} years old.\nMy shoesize is {small}");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
